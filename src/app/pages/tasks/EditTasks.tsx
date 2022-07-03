9aa616ef55cb2b89dfc1f704eff99c2a586f1c2f
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-07-03 16:31:16 -0300

    chore: dreated form for singup of new tasks

--- src/app/pages/tasks/EditTasks.tsx
+++ src/app/pages/tasks/EditTasks.tsx
@@ -1,21 +1,22 @@
 import { Box } from '@mui/material';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { ToolbarDetails } from '../../shared/components';
 import { LayoutBasePage } from '../../shared/layouts';
 
 export const EditTasks: React.FC = () => {
 	const { id = 'new' } = useParams<'id'>();
+	const navigate = useNavigate();
 
 	return (
 		<Box>
 			<LayoutBasePage
 				title={id === 'new' ? 'Nova tarefa' : 'Edição de tarefa'}
 				icon={id === 'new' ? 'add_circle' : 'edit'}
-				toolbar={<ToolbarDetails />}
+				toolbar={<ToolbarDetails
+					onClickInBack={() => navigate('/tasks')}
+				/>}
 			>
-				<Box>
-					Teste{id}
-				</Box>
+
 			</LayoutBasePage>
 		</Box>
 	);
--- src/app/shared/components/toolbar/ToolbarDetails.tsx
+++ src/app/shared/components/toolbar/ToolbarDetails.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon, IconButton, MenuItem, Paper, Select, TextField, Typography, useMediaQuery } from '@mui/material';
+import { Box, Button, Icon, IconButton, InputLabel, MenuItem, NativeSelect, Paper, Select, TextField, Typography, useMediaQuery } from '@mui/material';
 import { useTheme } from '@mui/system';
 
 interface IToolbarDetailsProps {
@@ -27,47 +27,78 @@ export const ToolbarDetails: React.FC<IToolbarDetailsProps> = (
 	const mdDown = useMediaQuery(theme.breakpoints.down('md'));
 
 	return (
-		<Box
-			width='100%'
-			height='500px'
-			component={Paper}
-			paddingY={1}
-			paddingX={1}
-			marginY={2}
-			elevation={4}
-		>
+		<>
 			<Box
-				display='flex'
-				flexDirection='column'
-				alignItems='center'
-				gap={3}
-				marginY={3}
+				width='100%'
+				height='500px'
+				component={Paper}
+				paddingY={1}
+				paddingX={1}
+				marginY={2}
+				elevation={4}
 			>
-				<TextField
-					sx={smDown ? { width: '90%' } : mdDown ? { width: '80%' } : { width: '50%' }}
-					label='Descrição da tarefa'
-				/>
-				<Select
-					sx={smDown ? { width: '90%' } : mdDown ? { width: '80%' } : { width: '50%' }}
-					value
-					label="Age"
+				<Box
+					display='flex'
+					flexDirection='column'
+					alignItems='center'
+					gap={3}
+					marginY={3}
 				>
-					<MenuItem>Ten</MenuItem>
-					<MenuItem>Twenty</MenuItem>
-					<MenuItem>Thirty</MenuItem>
-				</Select>
-			</Box>
-			<Box
-				display='flex'
-				justifyContent='center'
+					<TextField
+						sx={smDown ? { width: '90%' } : mdDown ? { width: '80%' } : { width: '50%' }}
+						label='Descrição da tarefa'
+					/>
+					<InputLabel id="demo-simple-select-label">Status</InputLabel>
+					<NativeSelect
+						variant='standard'
+						sx={smDown ? { width: '90%' } : mdDown ? { width: '80%' } : { width: '50%' }}
+						defaultValue={1}
+					>
+						<option value={1}>Pendente</option>
+						<option>Finalizado</option>
+					</NativeSelect>
+				</Box>
+				<Box
+					display='flex'
+					justifyContent='center'
 
-			>
-				<IconButton sx={{ display: 'flex', flexDirection: 'column' }}>
-					<Icon>upload</Icon>
-					<Typography variant='caption' sx={{ fontSize: '12px' }}>Imagem</Typography>
-				</IconButton>
-			</Box>
+				>
+					<IconButton sx={{ display: 'flex', flexDirection: 'column' }}>
+						<Icon>upload</Icon>
+						<Typography variant='caption' sx={{ fontSize: '12px' }}>Imagem</Typography>
+					</IconButton>
+				</Box>
 
-		</Box>
+				<Box
+					width='100%'
+					paddingY={1}
+					paddingX={1}
+					marginY={2}
+					marginTop={10}
+					display='flex'
+					justifyContent='center'
+					gap={3}
+				>
+					{showButtonSave &&
+						<Button
+							sx={{ borderRadius: '0' }}
+							variant='contained'
+							onClick={onClickInSave}
+						>
+							Salvar
+						</Button>
+					}
+					{showButtonBack &&
+						<Button
+							sx={{ borderRadius: '0' }}
+							variant='contained'
+							onClick={onClickInBack}
+						>
+							Voltar
+						</Button>
+					}
+				</Box>
+			</Box>
+		</>
 	);
 };
